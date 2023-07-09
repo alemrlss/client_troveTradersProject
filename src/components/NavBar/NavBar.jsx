adbba2dc0c0923263a9ca220a724a02cbca7f1c5
Alejandro <alejandroaml0528@gmail.com>
2023-07-09 16:44:54 -0400

    notifications in navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,26 +1,51 @@
 import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
 import { getDataUser } from "../../services/Auth";
 import { useAuthContext } from "../../contexts/authContext";
-import img from "../../assets/logo.png"
-
-
+import img from "../../assets/logo.png";
+import axios from "axios";
 function NavBar() {
-  const { logout } = useAuthContext()
+  const { logout } = useAuthContext();
   const idUser = getDataUser().id;
 
-  const handleLogout = ()=>{
-   logout()
-  }
+  const [notifications, setNotifications] = useState([]);
+  const [isOpen, setIsOpen] = useState(false);
+
+  const loadNotifications = () => {
+    // Realiza la solicitud al servidor para obtener las notificaciones
+    // Puedes usar axios o cualquier otra librería para hacer la solicitud HTTP
+    axios
+      .get(`http://localhost:3001/notifications/${idUser}`)
+      .then((response) => {
+        // Actualiza el estado con las notificaciones obtenidas del servidor
+        setNotifications(response.data);
+        console.log(response.data);
+      })
+      .catch((error) => {
+        // Manejo de errores si la solicitud falla
+        console.log(error);
+      });
+  };
+
+  const handleNotificationsClick = () => {
+    if (isOpen) {
+      setIsOpen(!isOpen);
+    } else {
+      setIsOpen(!isOpen);
+      loadNotifications();
+    }
+    console.log(isOpen);
+  };
+
+  const handleLogout = () => {
+    logout();
+  };
 
   return (
     <nav className="bg-white border-gray-200 dark:bg-gray-900">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         <a href="#" className="flex items-center">
-          <img
-            src={img}
-            className="h-16 mr-3"
-            alt="TroveTraders Logo"
-          />
+          <img src={img} className="h-16 mr-3" alt="TroveTraders Logo" />
           <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
             TroveTraders
           </span>
@@ -50,7 +75,15 @@ function NavBar() {
         <div className="hidden w-full md:block md:w-auto" id="navbar-default">
           <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
             <li>
-            <Link
+              <button
+                className="bg-blue-500 text-white px-4 py-2 rounded"
+                onClick={handleNotificationsClick}
+              >
+                Notificaciones
+              </button>
+            </li>
+            <li>
+              <Link
                 to={`/home`}
                 className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
                 aria-current="page"
@@ -58,16 +91,17 @@ function NavBar() {
                 Home
               </Link>
             </li>
-            <li >
-              <Link to={`/profile/${idUser}`}
+            <li>
+              <Link
+                to={`/profile/${idUser}`}
                 className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
               >
                 My Profile
               </Link>
             </li>
-            <li >
+            <li>
               <a
-              onClick={handleLogout}
+                onClick={handleLogout}
                 href="#"
                 className="py-2 pl-3 pr-4  text-red-500 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
               >
@@ -77,6 +111,23 @@ function NavBar() {
           </ul>
         </div>
       </div>
+      {isOpen && (
+        <div className="notification-menu absolute top-12 left-0 bg-white bordern border-gray-300 p-4 w-80 h-full overflow-y-scroll">
+          <h2 className="bg-green-500 p-2">Notificaciones</h2>
+          <ul className="divide-y divide-gray-300 ">
+            {notifications.map((notification) => (
+              <li
+                className={`bg-gray-300 p-4 m-1  ${
+                  notification.read === "true" ? "text-gray-500" : "text-black"
+                }`}
+                key={notification._id}
+              >
+                {notification.message}
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
     </nav>
   );
 }
